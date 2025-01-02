34138b51dbbdba18d52b3fb05a01af24efd14831
Dillon Jayatillake <dillonmichaeljay@gmail.com>
2025-01-02 22:44:40 +0530

    created constructor

--- src/Customer.java
+++ src/Customer.java
@@ -1,2 +1,14 @@
 public class Customer {
+    private String name;
+    private String username;
+    private String password;
+
+    // Customer
+    public Customer(String name, String username, String password){
+        this.name = name;
+        this.username = username;
+        this.password = password;
+    }
+
+    
 }
