64a1890105fd3302859bccb96a5fa55b3de4d5fd
Dillon Jayatillake <dillonmichaeljay@gmail.com>
2025-01-02 23:48:08 +0530

    added constructor
    added the getters and setters

--- src/PremiumCustomer.java
+++ src/PremiumCustomer.java
@@ -1,4 +1,21 @@
 public class PremiumCustomer extends Customer {
     // Additional attribute for premium customers
+    private double discountRate;
+
+    // Constructor to initialize PremiumCustomer attibutes
+    public PremiumCustomer(String name, String username, String password, double discountRate){
+        super(name, username, password);
+        this.discountRate = discountRate;
+    }
+
+    // Getters and Setters for discount rate
+    public double getDiscountRate() {
+        return discountRate;
+    }
+
+    public void setDiscountRate(double discountRate) {
+        this.discountRate = discountRate;
+    }
+
     
 }
