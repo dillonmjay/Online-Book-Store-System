82258e1026902f8b0cd826b0c846bd609c7a7623
Dillon Jayatillake <dillonmichaeljay@gmail.com>
2025-01-03 00:12:19 +0530

    added constructor

--- src/Order.java
+++ src/Order.java
@@ -1,2 +1,16 @@
 public class Order {
+    private int orderId;
+    private String bookTitle;
+    private int quantity;
+    private String customerName;
+
+    // Constructor
+    public Order(int orderId, String bookTitle, int quantity, String customerName){
+        this.orderId = orderId;
+        this.bookTitle = bookTitle;
+        this.quantity = quantity;
+        this.customerName = customerName;
+    }
+
+    
 }
