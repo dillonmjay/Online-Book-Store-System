03732a4aea7fb57d718bb27cd716abc4f32ebab0
Dillon Jayatillake <dillonmichaeljay@gmail.com>
2025-01-02 16:38:14 +0530

    Add getters and setters to book

--- src/Book.java
+++ src/Book.java
@@ -13,5 +13,37 @@ public class Book {
     }
 
     // Getters and Setters
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
     
 }
