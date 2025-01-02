6849cce059237f0289426ca57ffd436a2cad5466
Dillon Jayatillake <dillonmichaeljay@gmail.com>
2025-01-02 15:23:51 +0530

    created constructer

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,15 @@
 public class Book {
+    private String title;
+    private String author;
+    private double price;
+    private int quantity;
+
+    public Book(String title, String author, double price, int quantity) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
     
 }
